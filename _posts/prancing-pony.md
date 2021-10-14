190b203b243f9be0c755c9cfb08b74edb42f85e0
Carlos Gatti <eduardo.gf@hotmail.com>
2021-10-13 23:36:35 -0400

    Update full project

---
+++ _posts/prancing-pony.md
@@ -0,0 +1,16 @@
+---
+title: "Mining Crypto 3"
+excerpt: "Quer aprender como minerar crypto e ter uma renda extra?!"
+date: "2021-03-02"
+---
+
+Atualmente é muito simples iniciar um rig para minerar crypto, e o avanço desse tema
+fez com que os produtos e processos ficassem mais simples.
+
+Hoje qualquer pessoa com uma infrestrutura simples pode iniciar um rig de mineração
+com poucos minutos.
+
+Os equipamentos são caros e de dificil acesso, mas certamente com a nossa ajuda
+tudo ficará mais fácil, pois já passei por alguns problemas que podem ajuda-lo.
+
+Por isso decidi criar esse blog.
---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import Container from '../components/container'
+import Image from 'next/image'
+
+function HomePage() {
+  return (
+    <>
+      <Container>
+        <div className="space-y-6">
+          <h1 className="text-2xl font-bold">
+            I am a full stack developer with a passion for building and designing creative, responsive, web applications and tecnology. 
+          </h1>
+          <p>
+            Here I will gather information about all these subjects, and I intend to entice you to learn more about me and my projects at the moment.
+          </p>
+
+          <p>Consider taking a few minutes to watch, read, and get involved.</p>
+
+          <p>My name is <code><b>Carlos Gatti</b></code> and I'm happy to have you here, and now you just have to choose where you want to start.</p>
+        </div>
+      </Container>
+
+      <div className="container max-w-4xl m-auto px-4 mt-20">
+        <Image
+          src="/desk.jpg"
+          alt="my desk"
+          width={1920 / 2}
+          height={1280 / 2}
+        />
+      </div>
+    </>
+  )
+}
+
+export default HomePage
